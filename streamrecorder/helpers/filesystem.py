406f53a1db33c26e5c46cd80851ac67f47889703
Brinnyman <31727366+Brinnyman@users.noreply.github.com>
2019-02-15 19:42:38 +0100

    filesystem, create directory, create file

---
+++ streamrecorder/helpers/filesystem.py
@@ -0,0 +1,36 @@
+import os
+import datetime
+
+
+class Filesystem:
+    def __init__(self):
+        self.directory = ''
+        self.filename = ''
+
+    # create directory
+    def create_directory(self, path, name):
+        # TODO: check if path is empty, set default path to the path of the main directory
+        # create directory with at the specified path
+        self.directory = os.path.join(os.path.abspath(path), name)
+
+        if(os.path.isdir(self.directory) is False):
+            os.makedirs(self.directory)
+
+    # get directory
+    def get_directory(self):
+        # return directory
+        return self.directory
+
+    # create file
+    def create_file(self, name, date):
+        # create file with specified name and extension
+        self.filename = name + '_' + date + ".mp4"
+        self.filename = "".join(x for x in self.filename if x.isalnum()
+                           or x in ["-", "_", "."])
+        self.filename = os.path.join(self.get_directory(), self.filename)
+        return self.filename
+
+    # get filename
+    def get_filename(self):
+        # return filename
+        return self.filename
